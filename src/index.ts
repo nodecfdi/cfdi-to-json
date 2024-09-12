04cd51f2e7953af7661dcee1ed1f0615b3d2f834
Fernando Isidro Luna <luffynando@gmail.com>
2024-09-12 01:02:01 -0600

    build: :fire: prepare exports and config for prerelease new version

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { default as CfdiToDataNode } from '#src/cfdi_to_data_node';
+export { default as Factory } from '#src/factory';
+export { default as JsonConverter } from '#src/json_converter';
+export { default as Children } from '#src/nodes/children';
+export { default as KeysCounter } from '#src/nodes/keys_counter';
+export { default as Node } from '#src/nodes/node';
+export { default as defaultPaths } from '#src/source/default_paths';
+export { default as UnboundedOccursPaths } from '#src/unbounded_occurs_paths';
